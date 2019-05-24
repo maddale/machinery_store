b73120e671a4b0cf28b1c9f0d25dbecadb2c3add
Anatoly Shvets <madshvets@gmail.com>
2019-05-24 17:24:27 +0800

    Initializing

---
+++ js/main.js
@@ -0,0 +1,98 @@
+var merch = JSON.parse(data).data.products;
+
+var vm = new Vue({
+  el: "#vm",
+  data: {
+    items: merch,
+    filter: false,
+    modalVisible: false,
+    currentItem: "",
+    order: {},
+    cart: [],
+    modalMode: ""
+
+  },
+  computed: {
+    getUniqCats: function() {
+      var store = [];
+      var uniq_cats = [];
+      this.items.map(function(item) { store.push(item.machine_categories.map(function(cat){ return cat.name }))});
+      store = store.flat(1);
+      store.forEach(function(el){
+        if (uniq_cats.indexOf(el) == -1) {
+          uniq_cats.push(el)
+        }
+      });
+      return uniq_cats
+    },
+    getFilteredItems: function() {
+      return this.filter ? this.items.filter(item => item.machine_categories.map(function(ctg) { return ctg.name }).indexOf(this.filter) > -1 ) : this.items
+    },
+    cartCounter: function() {
+      return this.cart.reduce(function(sum, cur) { return sum + cur.count }, 0)
+    },
+    totalPrice: function() {
+      return this.cart.reduce(function(sum, cur) { return sum + (cur.count * cur.price) }, 0)
+    }
+  },
+
+  methods: {
+    openItem: function(item) {
+      this.modalMode    = "item";
+      this.currentItem  = item;
+      this.modalVisible = true
+    },
+    openCart: function() {
+      this.modalMode    = "order";
+      this.modalVisible = true
+    },
+    dec: function (id) {
+      var item = this.cart.findById(id);
+      if (item.count > 1) {
+        item.count -= 1
+      }
+    },
+    getNames: function(obj) {
+      return obj.map(function(el) { return el.name }).join(", ")
+    },
+    getItemById: function(id) {
+      return this.items.filter(function(item) { return item.id == id })[0]
+    },
+
+    addToCart: function(item) {
+      var cartItem = this.cart.findById(item.id)
+
+      if (cartItem) {
+        cartItem.count += 1
+      } else {
+        cartItem = {
+          id: item.id,
+          image: item.thumbnail.thumbnail_url,
+          name: item.name,
+          price: item.price,
+          count: 1,
+        // subTotal: item.price * count
+        };
+        this.cart.push(cartItem)
+      }
+    },
+
+    clearCart: function() {
+      while (this.cart.length > 0) {
+        this.cart.pop()
+      }
+    },
+
+    removeById: function(id) {
+      this.cart.forEach((el, i) => {
+        if (el.id == id) {
+          this.cart.splice(i, 1)
+        }
+      })
+    }
+  }
+});
+
+vm.cart.findById = function(id) {
+  return this.filter(function(item) { return item.id == id })[0]
+};
